153aa969fab6c69480281aeef82497bdc3c5b064
Hector Rios <that.hector@gmail.com>
2026-05-19 23:13:48 -0500

    feat(treesitter): Add configuration for languages

--- plugin/nvim-treesitter.lua
+++ plugin/nvim-treesitter.lua
@@ -13,3 +13,43 @@ vim.api.nvim_create_autocmd('PackChanged', {
     end
   end,
 })
+
+local treesitter = require('nvim-treesitter')
+local ensure_installed = {
+  'bash',
+  'diff',
+  'dockerfile',
+  'editorconfig',
+  'git_config',
+  'git_rebase',
+  'gitcommit',
+  'gitignore',
+  'hcl',
+  'json',
+  'lua',
+  'markdown',
+  'markdown_inline',
+  'python',
+  'ruby',
+  'rust',
+  'toml',
+  'vim',
+  'vimdoc',
+  'yaml',
+}
+
+treesitter.setup({})
+treesitter.install(ensure_installed)
+
+for _, parser in ipairs(ensure_installed) do
+  local filetypes = parser
+
+  vim.treesitter.language.register(parser, filetypes)
+
+  vim.api.nvim_create_autocmd({ 'FileType' }, {
+    pattern = filetypes,
+    callback = function(event)
+      vim.treesitter.start(event.buf, parser)
+    end,
+  })
+end
