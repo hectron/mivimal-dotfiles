83036d2e7b9d8db97a2fb0088816d86fc45a4faa
Hector Rios <that.hector@gmail.com>
2026-05-22 17:03:22 -0500

    feat(keymaps): better completion, news keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,5 +1,21 @@
 -- Escape insert mode with jj
-vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set('i', 'jj', '<Esc>')
+
+-- <CR> nearest completion option
+vim.keymap.set('i', '<CR>', function ()
+  if vim.fn.pumvisible() == 0 then
+    return '<CR>'
+  end
+
+  if vim.fn.complete_info({ 'selected' }).selected == -1 then
+    return '<C-n><C-y>'
+  end
+
+  return '<C-y>'
+end, { expr = true, desc = 'Completion: Accept completion or new line'})
+
+-- Neovim News
+vim.keymap.set('n', '<leader>N', '<cmd>help news<cr>', { desc = 'Neovim [N]ews' })
 
 vim.keymap.set('n', '<leader>Pl', function()
   vim.pack.update(nil, { offline = true })
