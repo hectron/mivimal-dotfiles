c6bec356427e5076b47be81eac763108a0745f0b
Hector Rios <that.hector@gmail.com>
2026-08-25 22:26:51 -0500

    feat(pack): Clean up old plugins; wrap mini plugins

--- plugin/mini.lua
+++ plugin/mini.lua
@@ -1,8 +1,12 @@
+local mini = function(plugin)
+  return "https://github.com/nvim-mini/" .. plugin
+end
+
 vim.pack.add({
-  "https://github.com/nvim-mini/mini.ai",
-  "https://github.com/nvim-mini/mini.icons",
-  "https://github.com/nvim-mini/mini.pairs",
-  "https://github.com/nvim-mini/mini.surround",
+  mini("mini.ai"),
+  mini("mini.icons"),
+  mini("mini.pairs"),
+  mini("mini.surround"),
 })
 
 require("mini.ai").setup()
